5a12353c09dc38d3f49c59a232596e5bf7a2c531
aktora <aktora@e4519edf-64a7-4029-8cd4-acf8db85d3f4>
2015-04-06 17:42:02 +0000

    changement de l'algo de la méthode samePercent
    git-svn-id: https://svn.u-cergy.fr/usvn/svn/glp2015_jmusic_cmi/trunk@13 e4519edf-64a7-4029-8cd4-acf8db85d3f4

---
+++ src/melody/tools/ProbabilityTable.java
@@ -0,0 +1,71 @@
+package melody.tools;
+
+import java.util.ArrayList;
+
+public class ProbabilityTable {
+	
+	private ArrayList<Integer> proba;
+	
+	public ProbabilityTable()
+	{
+		proba = new ArrayList<Integer>();
+	}
+
+	@Override
+	public String toString() {
+		return "ProbabilityTable [proba=" + proba + "]";
+	}
+
+	public ArrayList<Integer> getProba() {
+		return proba;
+	}
+
+	public void setProba(ArrayList<Integer> proba) {
+		this.proba = proba;
+	}
+	
+	public void add(Integer probability)
+	{
+		proba.add(probability);
+	}
+	
+	public void remove(int index)
+	{
+		proba.remove(index);
+	}
+	
+	public void removeFirst()
+	{
+		proba.remove(0);
+	}
+	
+	public void samePercent(Integer percent)
+	{
+		while(total()<100-percent)
+		{
+			proba.add(percent);
+		}
+		
+		if(!centPercent())
+		{
+			int rest = 100-total();
+			proba.add(rest);
+		}
+	}
+	public int total()
+	{
+		int total=0;
+		
+		for(int i=0;i<proba.size();i++)
+		{
+			total=total+proba.get(i);
+		}
+		
+		return total;
+	}
+	
+	public boolean centPercent()
+	{
+		return (total()==100);
+	}
+}
